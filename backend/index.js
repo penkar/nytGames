34013d2134adb1c11c8d5f48910d5e5cd095f851
JSP <jspenkar@gmail.com>
2024-03-20 20:53:07 -0500

    Update dictionary & add binary search

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,24 @@
-import en from "dictionary-en";
 import express from "express";
-import nspell from "nspell";
+import { binarySearchWordList } from "./binarySearch.js";
+
 const app = express();
 const port = 5000;
-
-const spell = nspell(en);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get("/:guess", (req, res) => {
+app.get("/g/:guess", (req, res) => {
   const guess = req.params.guess;
-  const correctSpelling = spell.correct(guess);
+  const correctSpelling = binarySearchWordList(guess);
+  console.log({ guess, correctSpelling });
 
   res.send({ guess, correctSpelling });
 });
 
 app.post("/guess", (req, res) => {
   const guess = req.body.guess;
-  const correctSpelling = spell.correct(guess);
+  const correctSpelling = binarySearchWordList(guess);
 
   res.send({ guess, correctSpelling });
 });
