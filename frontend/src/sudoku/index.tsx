3894d4757b5cdb08f4f4e08070ec45dbd5353642
JSP <jspenkar@gmail.com>
2024-04-20 23:22:48 -0500

    Name space the sudoku styles

--- frontend/src/sudoku/index.tsx
+++ frontend/src/sudoku/index.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import { Board } from "./components/Board";
 import { BoardProvider } from "./contexts/useBoard";
 import { ControlPanel } from "./components/ControlPanel";
-import { Header } from "./components/Header";
-import { Menu } from "./components/Menu";
+// import { Header } from "./components/Header";
+// import { Menu } from "./components/Menu";
 import { Numpad } from "./components/Numpad";
 
 import "./App.css";
@@ -13,11 +13,11 @@ function App() {
   return (
     <BoardProvider>
       <>
-        <Header />
+        {/* <Header /> */}
         <Board />
         <ControlPanel />
         <Numpad />
-        <Menu />
+        {/* <Menu /> */}
       </>
     </BoardProvider>
   );
